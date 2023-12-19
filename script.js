037bb781199e4c00394ad0e36e61d35432afdcd9
aru <110044109+im-aru@users.noreply.github.com>
2023-12-19 08:50:10 -0800

    Finished user resizable grid within fixed boundaries & made canvas a fixed size
    Stops potentially overwhelming grid sizes while still being user adjustable

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const containerDiv = document.querySelector(".container");
 const resizeButton = document.querySelector("button");
-containerDiv.appendChild(resizeButton);
 
 resizeGrid(16);
 
 function resizeGrid(sizeInDivs) {
+  removeOldGrid();
   for (let i = 0; i < sizeInDivs; i++) {
     const divGridRow = document.createElement("div");
     divGridRow.classList.add("row", "grid");
@@ -24,12 +24,22 @@ function makeGridDynamic() {
   divGrid.forEach(function (elem) {
     elem.addEventListener("mouseover", () => {
       elem.style.background = "pink";
-      console.log("Hit");
     });
   });
 }
+
+function removeOldGrid() {
+  const oldGrid = document.querySelectorAll(".grid");
+  oldGrid.forEach(function (elem) {
+    elem.remove();
+  });
+}
+
 resizeButton.addEventListener("click", () => {
   let userInput = prompt("How many squares would you like? (1-100)");
-  if (!Number(userInput) || !(0 < userInput <= 100)) break;
-  console.log(typeof userInput);
+  if (!Number(userInput) || !(0 < userInput, userInput <= 100)) {
+    alert("error: please input a number between 1 and 100.");
+    return;
+  }
+  resizeGrid(userInput);
 });
