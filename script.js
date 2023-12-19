59fd3e7e56ab0c485bb79ac5b492e04887c6425d
aru <110044109+im-aru@users.noreply.github.com>
2023-12-18 19:05:49 -0800

    Refactored to make sizing based on user input

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const containerDiv = document.querySelector(".container");
+const resizeButton = document.querySelector("button");
+containerDiv.appendChild(resizeButton);
 
-console.log(containerDiv);
+resizeGrid(16);
 
-for (let i = 0; i < 16; i++) {
-  const divGridRow = document.createElement("div");
-  divGridRow.className = "row"
-  containerDiv.appendChild(divGridRow);
+function resizeGrid(sizeInDivs) {
+  for (let i = 0; i < sizeInDivs; i++) {
+    const divGridRow = document.createElement("div");
+    divGridRow.classList.add("row", "grid");
+    containerDiv.appendChild(divGridRow);
 
-  for (let i = 0; i < 16; i++) {
-    const divGridCol = document.createElement("div");
-    divGridCol.className = "col";
-    divGridRow.appendChild(divGridCol);
+    for (let i = 0; i < sizeInDivs; i++) {
+      const divGridCol = document.createElement("div");
+      divGridCol.classList.add("col", "grid");
+      divGridRow.appendChild(divGridCol);
+    }
   }
+  makeGridDynamic();
 }
+
+function makeGridDynamic() {
+  const divGrid = Array.from(document.querySelectorAll(".grid"));
+  divGrid.forEach(function (elem) {
+    elem.addEventListener("mouseover", () => {
+      elem.style.background = "pink";
+      console.log("Hit");
+    });
+  });
+}
+resizeButton.addEventListener("click", () => {
+  let userInput = prompt("How many squares would you like? (1-100)");
+  if (!Number(userInput) || !(0 < userInput <= 100)) break;
+  console.log(typeof userInput);
+});
